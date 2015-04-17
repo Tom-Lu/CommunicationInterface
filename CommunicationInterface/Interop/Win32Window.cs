fb14c4833a3060981af1c76ea5bc1e04bba279e5
Tom Lu <luqiang1983@gmail.com>
2015-04-17 10:13:52 +0800

    Add test project

---
+++ CommunicationInterface/Interop/Win32Window.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Communication.Interface.Interop
+{
+    public class Win32Window
+    {
+        private bool haveMainWindow = false;
+        private IntPtr mainWindowHandle = IntPtr.Zero;
+        private int processId = 0;
+
+        private delegate bool EnumThreadWindowsCallback(IntPtr hWnd, IntPtr lParam);
+
+        public static IntPtr GetCurrentProcessMainWindowHandle()
+        {
+            return new Win32Window().GetMainWindowHandle(Process.GetCurrentProcess().Id);
+        }
+
+        private IntPtr GetMainWindowHandle(int processId)
+        {
+            if (!this.haveMainWindow)
+            {
+                this.mainWindowHandle = IntPtr.Zero;
+                this.processId = processId;
+                EnumThreadWindowsCallback callback = new EnumThreadWindowsCallback(this.EnumWindowsCallback);
+                EnumWindows(callback, IntPtr.Zero);
+                GC.KeepAlive(callback);
+
+                this.haveMainWindow = true;
+            }
+            return this.mainWindowHandle;
+        }
+
+        private bool EnumWindowsCallback(IntPtr handle, IntPtr extraParameter)
+        {
+            int num;
+            GetWindowThreadProcessId(new HandleRef(this, handle), out num);
+            if ((num == this.processId) && this.IsMainWindow(handle))
+            {
+                this.mainWindowHandle = handle;
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsMainWindow(IntPtr handle)
+        {
+            return (!(GetWindow(new HandleRef(this, handle), 4) != IntPtr.Zero) && IsWindowVisible(new HandleRef(this, handle)));
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool EnumWindows(EnumThreadWindowsCallback callback, IntPtr extraData);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(HandleRef handle, out int processId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetWindow(HandleRef hWnd, int uCmd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool IsWindowVisible(HandleRef hWnd);
+
+    }
+}
---
+++ CommunicationInterfaceTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Communication.Interface.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create communication interface instance based on connection string: "Telnet:IP=127.0.0.1,Port=23"
+            // Connection String Example:
+            // SerialPort - "SerialPort:Port=COM5,BaudRate=115200,DataBits=8,Parity=None,StopBits=One"
+            // Telnet - "Telnet:IP=127.0.0.1,Port=23"
+            // Ssh - "Ssh:IP=192.168.1.100,Port=23,Username=admin, Password=admin"
+            // Tcp - "Tcp:IP=127.0.0.1,Port=23"
+            // You can use connection builder user interface to configure connection string "ShowCommunicationBuilder"
+
+            ICommunicationInterface CommInterface = CommunicationManager.InstanceInterface("Telnet:IP=127.0.0.1,Port=23", "LocalTest");
+
+            // Buffer update event handler for console applicaiton, for WinForm applicaiton you should use ShowCommunicationViewer to display the trace window
+            CommInterface.BufferUpdatedHandler += new OnBufferUpdatedEvent(CommInterface_BufferUpdatedHandler);
+            // Open communication interface
+            CommInterface.Open();
+            if (CommInterface.IsOpened)
+            {
+                CommInterface.Timeout = 10;
+                CommInterface.StopToken = "test>";
+                CommInterface.WaitForString("login:", 100);
+                CommInterface.WriteLine("test");
+                CommInterface.WaitForString("password:", 100);
+                Thread.Sleep(100);
+                CommInterface.WriteLineWaitToken("test");
+                CommInterface.WriteLineWaitToken("ipconfig");
+                string LocalIP = string.Empty;
+                string LocalIPv6 = string.Empty;
+                CommInterface.ReadBuffer.ReadString("IPv4  . . . . . . . . . . . . :", out LocalIP);
+                CommInterface.ReadBuffer.ReadString("IPv6 . . . . . . . . :", out LocalIPv6);
+                CommInterface.WriteLineWaitToken("arp -a");
+                string Mac = string.Empty;
+                CommInterface.ReadBuffer.ReadString("192.168.1.1", out Mac);
+                CommInterface.WriteLineWaitToken("ping 192.168.1.1");
+                CommInterface.ReadBuffer.ReadString(@"IPv4\s*.*:", out Mac);
+
+                Console.WriteLine("Press any key continue ...");
+                Console.ReadKey();
+            }
+
+            // Close communication interface
+            CommInterface.Close();
+            CommInterface = null;
+        }
+
+        static void CommInterface_BufferUpdatedHandler(string Buffer)
+        {
+            Console.Write(Buffer);
+        }
+    }
+}
